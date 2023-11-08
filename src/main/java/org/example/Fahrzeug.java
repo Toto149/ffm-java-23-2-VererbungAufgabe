b1a9cfb17a534a6e19214d230bf12c5738695459
Thorsten Thomann <thorsten.thomann@googlemail.com>
2023-11-08 11:01:31 +0100

    Add "fahrzeugInformation"-method, which returns a String of information about the vehicle

--- src/main/java/org/example/Fahrzeug.java
+++ src/main/java/org/example/Fahrzeug.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Fahrzeug {
     String hersteller;
-    String Modell;
+    String modell;
     int baujahr;
+    public Fahrzeug(){
+
+    }
+    public Fahrzeug(String hersteller, String modell, int baujahr){
+        this.hersteller = hersteller;
+        this.modell = modell;
+        this.baujahr = baujahr;
+
+    }
+    public String fahrzeugInformationen(){
+        return "Der Hersteller ist %s, das Modell ist %d und das baujahr ist %r".formatted(hersteller,modell, baujahr);
+
+    }
 }
